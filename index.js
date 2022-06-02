d9303056a88d0a38aa15a8770b98de30c89648a1
zwb800 <zwb800@gmail.com>
2022-06-02 21:58:33 +0800

    实现

--- index.js
+++ index.js
@@ -15,14 +15,56 @@ const opts = {
   }
 };
 
-async function main () {
-  const client = await wdio.remote(opts);
+const notSend = ['燕子','ice bear','乖乖虎']
 
-  const field = await client.$$("id=com.tencent.mm:id/j9q");
-  console.log(field)
-  for (const e of field) {
-      e.touchAction('tap')
+function swipe(){
+  driver.touchAction([
+    {action: 'press', x: 583, y: 2000},
+    {action: 'moveTo', x: 583, y: 600},
+    'release'
+  ]);
+}
+
+let driver = null
+
+async function main () {
+  driver = await wdio.remote(opts);
+  
+  const btnNext = await driver.$('id=com.tencent.mm:id/em')
+
+  while(true){
+    const field = await driver.$$("id=com.tencent.mm:id/j9q")
+    if(field.length==0){
+      console.error('未找到元素')
+      break
+    }
+  
+    for (const e of field) {
+
+      const isChecked = await e.getAttribute("checked") == 'true'
+      if(isChecked){
+        continue
+      } 
+      const parent = await e.nextElement()
+      const text = await (await parent.$('id=com.tencent.mm:id/c8_')).getText()
+      if(!notSend.includes(text)){
+        await e.click()
+        const btnLabel = await btnNext.getText()
+        const regex = /\d+/.exec(btnLabel)
+        if(regex.length>0){
+          const count = parseInt(regex[0])
+          if(count == 200){
+            btnNext.click()
+            break
+          }
+          
+        }
+      }
+        
+    }
+    swipe()
   }
+  
 //   await field.setValue("Hello World!");
 //   const value = await field.getText();
 //   assert.strictEqual(value,"Hello World!");
